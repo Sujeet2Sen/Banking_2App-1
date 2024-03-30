a6d2e2e198e129bb78e793e090fe54f05e1188c2
sujeet <sensujeet2@gmail.com>
2024-03-30 11:41:01 +0530

    Banking Application

---
+++ Banking/src/main/java/sujeet/DBIntializer.java
@@ -0,0 +1,7 @@
+package sujeet;
+public interface DBIntializer {
+	String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	String CON_STRING = "jdbc:oracle:thin:@localhost:1521:xe";
+	String USERNAME = "system";
+	String PASSWORD = "Sujeet234";
+}
